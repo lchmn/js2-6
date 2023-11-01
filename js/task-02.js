5e17acc5c985db10a53e48f0d656150551dd74b5
Sviatoslav B <sviatoslavbilaniuk@gmail.com>
2023-11-01 13:50:06 +0100

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,17 +1,20 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
-const ingredientsUl = document.getElementById('ingredients');
+document.addEventListener('DOMContentLoaded', (event) => {
+    const ingredientsList = [
+      "Potatoes",
+      "Mushrooms",
+      "Garlic",
+      "Tomatos",
+      "Herbs",
+      "Condiments",
+    ];
 
-ingredientsList.forEach(ingredient => {
-  const li = document.createElement('li'); // Створення нового елемента li
-  li.textContent = ingredient; // Додавання текстового вмісту до li
-  li.classList.add('item'); // Додавання класу 'item' до li
-  
-  ingredientsUl.appendChild(li); // Додавання li до списку ul
+    const ingredientsUl = document.getElementById('ingredients');
+
+    ingredientsList.forEach(ingredient => {
+      const li = document.createElement('li'); 
+      li.textContent = ingredient; 
+      li.classList.add('item'); 
+
+      ingredientsUl.appendChild(li);
+    });
 });
