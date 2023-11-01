2d42cdcf3569b1efed169ee0670b5165668a7718
Sviatoslav B <sviatoslavbilaniuk@gmail.com>
2023-11-01 09:49:03 +0100

    second task

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ingredientsUl = document.getElementById('ingredients');
+
+ingredientsList.forEach(ingredient => {
+  const li = document.createElement('li'); // Створення нового елемента li
+  li.textContent = ingredient; // Додавання текстового вмісту до li
+  li.classList.add('item'); // Додавання класу 'item' до li
+  
+  ingredientsUl.appendChild(li); // Додавання li до списку ul
+});
