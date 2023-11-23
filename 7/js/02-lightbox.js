5a5ed1fd077797961b95819696e9a10bf75c6256
Sviatoslav B <sviatoslavbilaniuk@gmail.com>
2023-11-23 20:37:12 +0100

    Update 02-lightbox.js

--- 7/js/02-lightbox.js
+++ 7/js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+document.addEventListener('DOMContentLoaded', () => {
+  const galleryContainer = document.querySelector('.gallery');
+  const galleryMarkup = createGalleryMarkup(galleryItems);
+  galleryContainer.innerHTML = galleryMarkup;
+
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+});
+
+function createGalleryMarkup(items) {
+  return items.map(({ preview, original, description }) => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    </li>
+  `).join('');
+}
